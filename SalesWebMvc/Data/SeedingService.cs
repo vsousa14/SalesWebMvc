5a1db9bbdcfb052561c50ccf6a34ed95424983c2
Vasco Sousa <vasco.sousa14@hotmail.com>
2021-03-10 12:59:13 +0000

    Update seller and custom service exception

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Data
 
         public void Seed()
         {
-            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+                if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
             {
                 return; // DB is already populated
             }
